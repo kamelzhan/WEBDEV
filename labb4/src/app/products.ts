9127537fb723cac1ffd7818ec6e753aba2caa414
kamelzhan <k.kamelzhan2000@gmail.com>
2021-02-24 14:19:21 +0600

    lab4

---
+++ labb4/src/app/products.ts
@@ -0,0 +1,126 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Hoodies with The Japanese Anime Naruto Autumn Outerwear',
+    price: '$18.99 - $26.99',
+    rating: '4.1 out of 5',
+    description: 'We are Asian size, please choose your right size refer to our description below and size chart in our detailed drawings,or order one size up accordingly.'+
+    'Bettydom owns its own trademarks. Trademark Number: 86800105.'+
+    'Made by high quality polyester, lightweight, thickness, soft, comfortable and good air permeability.'+
+    '3D Cartoon Patterns Printed , Super Fashion Look. Nice gift to give as a Birthday present or Christmas gift for boys and girls,Japanese Anime fans .'+
+    'Ideal for Daily Wear, Casual, Sports Occasion, Couple Wear, Club Party Wear. Hand Wash Only in Low Temperature or Dry Washing, Please Dont Iron.',
+    image: 'https://m.media-amazon.com/images/I/514J9H5xybL._AC_UL480_FMwebp_QL65_.jpg',
+    url: 'https://www.amazon.com/Bettydom-Novelty-Sweatshirt-Japanese-Outerwear/dp/B07PMC8KK5/ref=sr_1_14?dchild=1&keywords=sweatshirts+naruto&qid=1614113841&s=fashion-mens-intl-ship&sr=1-14'
+  },
+  {
+    id: 2,
+    name: 'Unisex Naruto Hoodie Akatsuki',
+    price: '$25.99 - $31.99',
+    rating: '4.5 out of 5',
+    description: '♡ 100% Brand New And High Quality.Choose your right size with the reference on item Please check the size chart carefully in the product description before placing your order.'+
+    '♡ Material: Polyester/Spandex blend, stylish and high quality construction. Extremely comfortable to wear, lightweight and breathable.'+
+    '♡ Cartoon Print, Super Fashion Look. Nice gift to give as a Birthday present or Christmas gift for Men Women Teen'+
+    '♡ Great for cosplay,casual,sports,outdoor.If you are a fan of naruto,do not hesitate to pick it.'+
+    '♡ This unisex string hoodie features a 3D sublimated print! Colors do not fade and stay vibrant after washing!',
+    image: 'https://m.media-amazon.com/images/I/51+QPBEU2iL._AC_UL480_FMwebp_QL65_.jpg',
+    url: 'https://www.amazon.com/XSKJY-Akatsuki-Sweatshirt-Outerwear-Pullovers/dp/B08PD3FQVJ/ref=sr_1_3?dchild=1&keywords=sweatshirts+naruto&qid=1614113841&s=fashion-mens-intl-ship&sr=1-3'
+  },
+  {
+    id: 3,
+    name: 'Demon Slayer Hoodies',
+    price: '$19.99',
+    rating: '4.3 out of 5',
+    description: 'Demon Slayer Agatsuma Zenitsu Hoodies Cosplay Costume Sweatshirt Top Pullover for Adult Women'+
+    'Material:Polyester'+
+    'Including:A Agatsuma Zenitsu cosplay costume only'+
+    'Occasion:The costume perfect for daily wear, Halloween, theme party, cosplay, on the stage,etc.'+
+    'ATTENTION:Please check the size chart carefully before you purchase the costume.',
+    image: 'https://m.media-amazon.com/images/I/61Pdn15fC2L._AC_UL480_FMwebp_QL65_.jpg',
+    url: 'https://www.amazon.com/Agatsuma-Zenitsu-Hoodies-Sweatshirt-Pullover/dp/B07YPQG3F6/ref=sr_1_2?dchild=1&keywords=sweatshirts+demon+slayer&qid=1614114319&refresh=1&sr=8-2'
+  },
+  {
+    id: 4,
+    name: 'Demon Slayer Sweatshirt',
+    price: '$23.49-$30.99',
+    rating: '4.4 out of 5',
+    description: 'Made of High quality polyester and spandex. Soft and comfortable fabric. Super wear-resistant.'+
+    '3D printed unisex pullover.Soft, Stretchy, Breathable Fabric Provides Exceptional Comfort And Durability'+
+    'Great for casual daily wear. Match well with jeans, legging pants.'+
+    'Wardrobe Essential: Mix And Match Separates And Accessories To Create A Wardrobe Full Of Staple Pieces For Work, Life, And Everything In Between'+
+    'Please refer to SIZE CHART before you place your order',
+    image: 'https://m.media-amazon.com/images/I/61YretLDr3L._AC_UL480_FMwebp_QL65_.jpg',
+    url: 'https://www.amazon.com/Lucky-Park-Demon-Slayer-Sweatshirt/dp/B07VMRLC67/ref=sr_1_13?dchild=1&keywords=sweatshirts+demon+slayer&qid=1614114418&refresh=1&sr=8-13'
+  },
+  {
+    id: 5,
+    name: 'Hanes Mens Pullover',
+    price: '$12.00 - $41.88',
+    rating: '4.6 out of 5',
+    description: '50% cotton/50% polyester, with up to 5% polyester created from recycled plastic.'+
+    'Imported.'+
+    'Pull On closure.'+
+    'Machine Wash.'+
+    'Soft, durable fleece with double-needle cover-seamed neck and armholes stays strong when you work or play hard.'+
+    'Machine wash it again and again without hesitation because its pill-resistant'+
+    'Made with up to 5% polyester created from recycled plastic bottles.'+
+    'Dyed-to-match drawcord.',
+    image: 'https://m.media-amazon.com/images/I/91wiANmkdhL._AC_UL480_FMwebp_QL65_.jpg',
+    url: 'https://www.amazon.com/Hanes-Pullover-Ecosmart-Sweatshirt-Charcoal/dp/B01L8JJY08/ref=sr_1_3?dchild=1&keywords=sweatshirts&qid=1614114654&sr=8-3'
+  },
+  {
+    id: 6,
+    name: 'THE SCOTTS Men Crewneck Sweatshirt',
+    price: '$38.99 - $39.99',
+    rating: '4.6 out of 5',
+    description: 'Comfortable Material:These crewneck sweatshirts feature the comfortable cotton and polyester fabric.Good air permeability,soft feeling,brings you a comfortable experience',
+    image: 'https://m.media-amazon.com/images/I/71GJVhmtdtL._AC_UL480_FMwebp_QL65_.jpg',
+    url: 'https://www.amazon.com/SCOTTS-Crewneck-Sweatshirt-Ghosts-Graphic/dp/B08JC57DVJ/ref=sr_1_13?dchild=1&keywords=sweatshirts&qid=1614114756&sr=8-13'
+  },
+  {
+    id: 7,
+    name: 'Unisex Funny Graphic Printed Sweatshirts',
+    price: '$24.99 - $27.99',
+    rating: '4.4 out of 5',
+    description: 'All-over printed Hooded Sweatshirts are uniquely crafted using a special sublimation technique to transfer designs into a deep, rich and lasting piece of wearable art.',
+    image: 'https://m.media-amazon.com/images/I/61-wryHxK7L._AC_UL480_FMwebp_QL65_.jpg',
+    url: 'https://www.amazon.com/ZM-Graphic-Printed-Sweatshirts-Hoodies/dp/B0837B6C6J/ref=sr_1_6?dchild=1&keywords=sweatshirts+printed+for+men&qid=1614114889&sr=8-6'
+  },
+  {
+    id: 8,
+    name: 'Pandolah Mens Patterns Print 3D Sweaters',
+    price: '$20.88',
+    rating: '4.2 out of 5',
+    description: 'Vivid Printing&100%Non Fading: Fashion 3D digital LASER PRINTED HOODIES in the front and back,features drawstring hood, long sleeves, front with BIG POCKET Paint Hoodie which can keep your hands warm or carry many stuff.',
+    image: 'https://m.media-amazon.com/images/I/619sFPa6QRL._AC_UL480_FMwebp_QL65_.jpg',
+    url: 'https://www.amazon.com/Pandolah-Patterns-Sweaters-Sweatshirts-Cleaner-h/dp/B07KT1YKQH/ref=sr_1_15_sspa?dchild=1&keywords=sweatshirts+printed+for+men&qid=1614114959&sr=8-15-spons&psc=1&smid=AJY2UXGHX8L60&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExSVRQTUJSSDlKTzlZJmVuY3J5cHRlZElkPUEwMzA1MzgyMTc3MVQzNlhUVTRMTyZlbmNyeXB0ZWRBZElkPUEwMjU5NzE2MUY2U0dUTVNPS1Q4UiZ3aWRnZXROYW1lPXNwX210ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU='
+  },
+  {
+    id: 9,
+    name: 'Aelfric Eden Mens Long Sleeve',
+    price: '$28.99 - $48.99',
+    rating: '4.4 out of 5',
+    description: 'Cotton blend. Soft fabric.This womens sweater is 100% brand new and high quality! Our synthetic materials allow air to pass through and contain a hydrophobic, or “water-hating”, quality within the fibers of the material which transport sweat to drier areas away from the skin. so it is warm and comfortable and it also ensures the stable quality of the sweater.',
+    image: 'https://m.media-amazon.com/images/I/61wGRvdDOvL._AC_UL480_FMwebp_QL65_.jpg',
+    url: 'https://www.amazon.com/Aelfric-Eden-Pullover-Sweaters-Christmas/dp/B082KG6Q85/ref=sr_1_20?dchild=1&keywords=sweatshirts+printed+for+men&qid=1614114959&sr=8-20'
+  },
+  {
+    id: 10,
+    name: 'Champion Hoodie Men',
+    price: '$42.99 - $54.99',
+    rating: '4.6 out of 5',
+    description: 'Champion Hoodie Men Big And Tall Hoodies For Men Pullover Champion Sweatshirt keeps you warm',
+    image: 'https://m.media-amazon.com/images/I/71n1LucdU7L._AC_UL480_FMwebp_QL65_.jpg',
+    url: 'https://www.amazon.com/Champion-Mens-Pullover-Fashion-Hoodie/dp/B07H5SL4HW/ref=sr_1_24?dchild=1&keywords=sweatshirts+printed+for+men&qid=1614114959&sr=8-24'
+  },
+
+
+
+
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
