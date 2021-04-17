9c59573dfc4dbd468797efdc12bcf62536c91952
kamelzhan <k.kamelzhan2000@gmail.com>
2021-04-17 18:13:10 +0600

    lab8

--- lab5/src/app/type.ts
+++ lab5/src/app/type.ts
@@ -6,17 +6,17 @@ export const type = [
     },
     {
         name: 'Demon Slayer Hodies',
-        type:2,
+        type: 2,
         img: 'https://m.media-amazon.com/images/I/71O7n4MymFL._AC_UL480_FMwebp_QL65_.jpg',
     },
     {
         name: 'Rick and Morty Hodies',
-        type:3,
+        type: 3,
         img: 'https://m.media-amazon.com/images/I/71tNLySIcIL._AC_UL480_FMwebp_QL65_.jpg',
     },
     {
         name: 'Usual Hodies',
-        type:4,
+        type: 4,
         img: 'https://m.media-amazon.com/images/I/91wiANmkdhL._AC_UL480_FMwebp_QL65_.jpg',
     },
 ];
